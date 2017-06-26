07371b33bdc073404427e2376104ceb0550260d2
Angus Schuler <asc132@uclive.ac.nz>
2017-06-26 17:32:26 +1200

    Initial Commit

---
+++ safetycheck.js
@@ -0,0 +1,28 @@
+// Created for MYOB IT Challenge at UC, 2017 by Stephan Hofmann
+
+var businessname
+var employeenum
+var ceoname
+var hazards
+var mgrrole
+var riskrating
+var risklikely
+var totalrisk
+
+function calculateRisk() {
+  totalrisk = risklikely*riskrating
+}
+
+function createPlan() {
+"1. Employer commitment to health and safety
+
+We are committed to making this workplace safe and healthy.
+
+This section will help you achieve the requirements for WSMP Element 1.
+
+
+1.1 Health and safety policy
+
+Management commitment"
+ceoname & "has signed our health and safety policy, demonstrating the commitment of management to the health and safety of people working or involved with our organisation. The policy outlines the duties of management and employees. A copy of our health and safety policy is on the next page."
+}
---
+++ submit.php
@@ -0,0 +1,22 @@
+<html>
+<body>
+Thanks! Download your health and safety plan here.
+
+<a href="/hsplan-jgplumbingco.doc" download>
+
+Your data:
+<?php echo $_POST["name"]; ?><br>
+Authority Name: <?php echo $_POST["mgrname"]; ?>
+Number of employees: <?php echo $_POST["numberemploy"]; ?>
+Hazard 1: <?php echo $_POST["h1"]; ?>
+Hazard 2: <?php echo $_POST["h2"]; ?>
+Hazard 3: <?php echo $_POST["h3"]; ?>
+Hazard 4: <?php echo $_POST["h4"]; ?>
+Strategy 1: <?php echo $_POST["s1"]; ?>
+Strategy 2: <?php echo $_POST["s2"]; ?>
+Strategy 3: <?php echo $_POST["s3"]; ?>
+Strategy 4: <?php echo $_POST["s4"]; ?>
+
+
+</body>
+</html>
